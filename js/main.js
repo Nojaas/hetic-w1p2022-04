ab9e0441850c3653971508e4a1f644b718c1370c
OrosGabriel <orosgabriel17@gmail.com>
2019-11-27 16:07:01 +0100

    New Mobile

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var opener = document.getElementById('burger')
+var exit = document.getElementById('exit')
+var menu = document.getElementById('menu');
+opener.addEventListener('click', function () {
+    menu.classList.toggle('ap')
+    exit.classListm.toggle('exit')
+})
