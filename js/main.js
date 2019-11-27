6be9a03347fef9ae24229767bf1fe547b50d42b6
OrosGabriel <orosgabriel17@gmail.com>
2019-11-27 16:56:06 +0100

    Burger Effect

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-var opener = document.getElementById('burger')
-var exit = document.getElementById('exit')
+var opener = document.getElementById('burger');
+var x = document.getElementById('exit');
+var y = document.getElementById('exit2');
+var z = document.getElementById('exit3');
+
 var menu = document.getElementById('menu');
+
 opener.addEventListener('click', function () {
-    menu.classList.toggle('ap')
-    exit.classListm.toggle('exit')
+    menu.classList.toggle('ap');
+    x.classList.toggle('cruce');
+    y.classList.toggle('cruce2');
+    z.classList.toggle('cruce3');
+
+
+
 })
