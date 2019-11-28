6f0895639ea5fb478b131ecbb798b8e5c55107a5
OrosGabriel <orosgabriel17@gmail.com>
2019-11-28 16:26:05 +0100

    smooth scroll :)

--- js/main.js
+++ js/main.js
@@ -16,3 +16,12 @@ opener.addEventListener('click', function () {
 
 })
 // ----------------------------------------------------------------
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
